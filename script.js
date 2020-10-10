0cc8194f1fd4c04d0b1d37ce148803f292f82fa4
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-09 19:50:16 -0600

    buttons

---
+++ script.js
@@ -0,0 +1,21 @@
+// set variables
+var startBtn = document.getElementById("start-btn")
+startBtn.addEventListener("click", startGame)
+
+//start game
+function startGame() {
+console.log("started")
+
+}
+
+//Next question
+function nextQuestion() {
+
+
+}
+
+//select answer
+function selectAnswer() {
+
+
+}
