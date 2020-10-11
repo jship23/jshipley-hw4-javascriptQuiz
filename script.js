bb12c8b3d86da3a15a125c0120896ff02444dc17
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-10 20:21:18 -0600

    more changes

--- script.js
+++ script.js
@@ -29,43 +29,122 @@ var questions = [
 
 // set variables
 var headEl = document.querySelector(".head");
-var next = document.getElementById("next-btn");
 var startBtn = document.getElementById("start-btn");
 var buttonEl = document.createElement("button");
 var containerEl = document.getElementsByClassName("container")
+var score = 0;
 
+// click start
 startBtn.addEventListener("click",startQuiz);
 
 //start quiz
 function startQuiz() {
-    
-    headEl.innerHTML = questions[0].question;
-    containerEl.append(headEl)
-    console.log(questions[0].question)
- 
-    
-    for(var i = 0; i < questions[0].choices.length; i++) {
-        buttonEl.innerText = questions[0].choices[i];
-        console.log(questions[0].choices[i]);
+//first question
+  headEl.innerHTML = questions[0].question;
+  console.log(questions[0].question);
+  startBtn.remove();
+
+  for (var i = 0; i < questions[0].choices.length; i++) {
+    var buttonEl = document.createElement("button");
+    buttonEl.innerHTML = questions[0].choices[i];
+    console.log(questions[0].choices[i]);
+    buttonEl.classList.add("answer-button");
+    headEl.append(buttonEl);
+  }
+
+  var nextBtn = document.createElement("button");
+  nextBtn.innerHTML = "Next";
+  nextBtn.classList.add("next-button");
+  headEl.append(nextBtn);
+
+  //next question
+  nextBtn.addEventListener("click", nextQuestion);
+  function nextQuestion() {
+    headEl.innerHTML = questions[1].question;
+    console.log(questions[1].question);
+    startBtn.remove();
+
+    for (var i = 0; i < questions[1].choices.length; i++) {
+      var buttonEl = document.createElement("button");
+      buttonEl.innerHTML = questions[1].choices[i];
+      console.log(questions[1].choices[i]);
+      buttonEl.classList.add("answer-button");
+      headEl.append(buttonEl);
+    }
+
+    var nextBtn = document.createElement("button");
+    nextBtn.innerHTML = "Next";
+    nextBtn.classList.add("next-button");
+    headEl.append(nextBtn);
+
+    //next question
+    nextBtn.addEventListener("click", nextQuestion);
+    function nextQuestion() {
+      headEl.innerHTML = questions[2].question;
+      console.log(questions[2].question);
+      startBtn.remove();
+  
+      for (var i = 0; i < questions[2].choices.length; i++) {
         var buttonEl = document.createElement("button");
+        buttonEl.innerHTML = questions[2].choices[i];
+        console.log(questions[2].choices[i]);
         buttonEl.classList.add("answer-button");
         headEl.append(buttonEl);
+      }
+
+      var nextBtn = document.createElement("button");
+      nextBtn.innerHTML = "Next";
+      nextBtn.classList.add("next-button");
+      headEl.append(nextBtn);
+
+
+      //next question
+      nextBtn.addEventListener("click", nextQuestion);
+      function nextQuestion() {
+        headEl.innerHTML = questions[3].question;
+        console.log(questions[3].question);
+        startBtn.remove();
+    
+        for (var i = 0; i < questions[3].choices.length; i++) {
+          var buttonEl = document.createElement("button");
+          buttonEl.innerHTML = questions[3].choices[i];
+          console.log(questions[3].choices[i]);
+          buttonEl.classList.add("answer-button");
+          headEl.append(buttonEl);
         }
+
+        var nextBtn = document.createElement("button");
+        nextBtn.innerHTML = "Next";
+        nextBtn.classList.add("next-button");
+        headEl.append(nextBtn);
+
+        //Next question
+        nextBtn.addEventListener("click", nextQuestion);
+        function nextQuestion() {
+          headEl.innerHTML = questions[4].question;
+          console.log(questions[4].question);
+          startBtn.remove();
       
+          for (var i = 0; i < questions[4].choices.length; i++) {
+            var buttonEl = document.createElement("button");
+            buttonEl.innerHTML = questions[4].choices[i];
+            console.log(questions[4].choices[i]);
+            buttonEl.classList.add("answer-button");
+            headEl.append(buttonEl);
+
+        }
+        //submit button
+        var submitBtn = document.createElement("button");
+        submitBtn.innerHTML = "Submit";
+        submitBtn.classList.add("next-button")
+        headEl.append(submitBtn);
+
+
+    }
+    }
+    }
+    }
+    
 };
-
-
-
-
-//select answer
-function selectAnswer() {
-
-}
-
-
-//Next question
-function nextQuestion() {
-
-}
 
 
